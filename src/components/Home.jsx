76b6f4e0127454568daf2b2dd45d1d5b55511350
Vishrut Sharma <vishrutsharma@192.168.1.7>
2024-03-20 00:56:26 +0530

    New Work Added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
                 <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a full stack web developer with 3+ years of experience. I create successful responsive websites that are fast, easy to use, and built with best practices. The main area of my expertise is front-end development with various frameworks such as React 
                     and also coding with HTML, CSS, JS building small and medium web apps, custom plugins, features, animations, and coding interactive layouts.</p>
                     <div>
-                        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'><Link to="work" smooth={true} duration={500}>View Work</Link>
+                        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'><Link to="work" smooth={true} duration={200}>View Work</Link>
                         <span className='group-hover:rotate-90 duration-300'><Link to="work" smooth={true} duration={500}>
                         <HiArrowNarrowRight className='ml-3'/></Link>
                         </span>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,81 +1,126 @@
-import React from 'react';
-import Expensify from '../assets/Expensify.png';
-import ChatApp from '../assets/ChatApp.png';
-import ContactApp from '../assets/ContactApp.png';
-import WorkImg from '../assets/workImg.jpeg';
+import React from "react";
+import Expensify from "../assets/Expensify.png";
+import ChatApp from "../assets/ChatApp.png";
+import ContactApp from "../assets/ContactApp.png";
+import FoodApp from "../assets/FoodApp.png";
+import WorkImg from "../assets/workImg.jpeg";
 
 const Work = () => {
   return (
-    <div name="work" className='w-full h-[950px] md:h-[350px] text-gray-300 bg-[#0a192f]'>
-        <div className='max-w-[1000px] mx-auto p-6 flex flex-col justify-center w-full h-full'>
-            <div className='mt-8 pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Projects</p>
-                <p className='py-6'>Check out some of my work :</p>
-            </div>
-
-            {/**Conatiner */}
-           
-            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                
-                {/**Grid Items */}
-                <div style={{backgroundImage: `url(${Expensify})`}}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    
-                    {/**Hover Effect */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                            Expensify Application                        </span>
-                        <div className='pt-8 text-center'>
-                            <a  target="_blank" href="https://expensifyourbudget.herokuapp.com/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Demo</button>
-                            </a>
-                            {/*<a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Code</button>
-                            </a>*/}
-                        </div>
-                    </div>
-                </div>
-                                
-                <div style={{backgroundImage: `url(${ChatApp})`}}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    {/**Hover Effect */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                            Chat Application
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a target="_blank" href="https://wannacatchup.herokuapp.com/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Demo</button>
-                            </a>
-                            {/*<a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Code</button>
-                            </a>*/}
-                        </div>
-                    </div>
-                </div>
-
-                <div style={{backgroundImage: `url(${ContactApp})`}}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    {/**Hover Effect */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className=' text-2xl font-bold text-white tracking-wider'>
-                            Contact Manager 
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a target="_blank" href="https://manage-my-contacts.herokuapp.com/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Demo</button>
-                            </a>
-                            {/*<a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Code</button>
-                            </a>*/}
-                        </div>
-                    </div>
-                </div>
-
-
-            
-
-            </div>
+    <div
+      name="work"
+      className="w-full h-[950px] md:h-[350px] text-gray-300 bg-[#0a192f]"
+    >
+      <div className="max-w-[1000px] mx-auto p-6 flex flex-col justify-center w-full h-full">
+        <div className="mt-8 pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Projects
+          </p>
+          <p className="py-6">Check out some of my work :</p>
         </div>
-    </div>
-  )
-}
 
-export default Work
+        {/**Conatiner */}
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
+          {/**Grid Items */}
+
+          <div
+            style={{ backgroundImage: `url(${FoodApp})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/**Hover Effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className=" text-2xl font-bold text-white tracking-wider">
+                Food Application
+              </span>
+              <div className="pt-8 text-center">
+                <a target="_blank" href="https://foodymouth.netlify.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    Demo
+                  </button>
+                </a>
+                {/*<a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Code</button>
+                            </a>*/}
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${Expensify})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/**Hover Effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Expensify Application{" "}
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  target="_blank"
+                  href="https://expensifyourbudget.herokuapp.com/"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    Demo
+                  </button>
+                </a>
+                {/*<a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Code</button>
+                            </a>*/}
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${ChatApp})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/**Hover Effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Chat Application
+              </span>
+              <div className="pt-8 text-center">
+                <a target="_blank" href="https://wannacatchup.herokuapp.com/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    Demo
+                  </button>
+                </a>
+                {/*<a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Code</button>
+                            </a>*/}
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${ContactApp})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/**Hover Effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className=" text-2xl font-bold text-white tracking-wider">
+                Contact Manager
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  target="_blank"
+                  href="https://manage-my-contacts.herokuapp.com/"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    Demo
+                  </button>
+                </a>
+                {/*<a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Code</button>
+                            </a>*/}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
