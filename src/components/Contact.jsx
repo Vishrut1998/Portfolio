6e085143bd2eb2040ed9e02eb43ed394c80c833b
Vishrut Sharma <vishrutsharma@192.168.1.13>
2025-03-15 22:36:07 +0530

    icons_resume_code update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,8 @@ import {BsPhone} from 'react-icons/bs';
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-[90vh] bg-[#0a192f] flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/f3ebe8bb-a653-4e6d-a884-fbbe08bec439" className='mb-20 flex flex-col max-w-[600px] w-full'>
+       <form method='POST' action="https://getform.io/f/f3ebe8bb-a653-4e6d-a884-fbbe08bec439" className='mb-[10.5rem] md:mt-[90vh] flex flex-col max-w-[600px] w-full'>
+
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-2xl font-bold text-gray-300 py-4'>Submit the form or contact me :</p>
@@ -15,7 +16,7 @@ const Contact = () => {
             <input className="bg-[#ccd6f6] p-2" type="text" placeholder='Your Name' name="Name"/>
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Your Email' name="Email"/>
             <textarea className='p-2 bg-[#ccd6f6]' name="message" rows="10" placeholder='Message'/>
-            <button className='text-white border-2 hover:bg-pink-600 hover-border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+            <button className='text-white border-2 hover:bg-pink-600 hover-border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Connect</button>
         </form>
     </div>
   )
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -11,7 +11,7 @@ const Work = () => {
       name="work"
       className="w-full h-[950px] md:h-[350px] text-gray-300 bg-[#0a192f]"
     >
-      <div className="max-w-[1000px] mx-auto p-6 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] mx-auto p-6 flex flex-col justify-center w-full h-full cursor-pointer">
         <div className="mt-[15rem] pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Projects
