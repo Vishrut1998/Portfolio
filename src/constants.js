bcde0d60454b00ee7c6cb2d2d832d2fceb313c9a
Vishrut Sharma <vishrutsharma@192.168.1.13>
2025-03-16 19:21:46 +0530

    Resume-Update

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const RESUME_LINK = 'https://acrobat.adobe.com/id/urn:aaid:sc:AP:c5ef3fba-5bb3-481b-b430-aa6ccf11ea62'
+export const RESUME_LINK = 'https://acrobat.adobe.com/id/urn:aaid:sc:AP:d35a573c-444a-41ec-a0b8-586412111e94'
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/vishrut-sharma-448588166'
 export const GITHUB_URL = 'https://github.com/Vishrut1998'
